10b5651235a8a498e8183460ec06738bb6145e61
Tim Patterson <timp@posteo.net>
2020-05-10 13:14:17 -0400

    Rock paper scissors game

--- game.js
+++ game.js
@@ -7,37 +7,91 @@ function randomPlay() {
 	return options[randomInt(3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+// 1 = player win
+// 2 = computer win
+// 3 = tie
+function checkWin(playerSelection, computerSelection) {
 	playerSelection = playerSelection.toLowerCase();
-	
+
 	if (playerSelection === computerSelection) {
-		return "Tie! " + playerSelection;
+		return 3; // tie
 	}
 
-	let win = false;
+	let win = 2;
 
 	switch (playerSelection) {
 		case "rock":
-		if (computerSelection === "scissors") win = true;
+		if (computerSelection === "scissors") win = 1;
 		break;
 
 		case "paper":
-		if (computerSelection === "rock") win = true;
+		if (computerSelection === "rock") win = 1;
 		break;
 
 		case "scissors":
-		if (computerSelection === "paper") win = true;
+		if (computerSelection === "paper") win = 1;
 		break;
 
 		default:
-		return "Invalid input"
+		return 2; // invalid input means lose
 	}
 
+	return win;
+}
+
+function playRound(playerSelection, computerSelection) {
 	let result = "lose!";
 	if (win) result = "win!";
 
-	return "You " + result + " You: " + playerSelection +
-		" Computer: " + computerSelection;
+	return "You " + result + "\nYou: " + playerSelection +
+		"\nComputer: " + computerSelection;
 }
 
-console.log(playRound(randomPlay(), randomPlay()));
+function playGame(rounds) {
+	let wins = 0;
+	let loses = 0;
+	let ties = 0;
+
+	for (ii = 0; ii < rounds; ii++) {
+		console.log("Round " + (ii + 1) + "...");
+
+		playerInput = prompt("Rock, paper, or scissors?");
+		computerInput = randomPlay();
+
+		let result = checkWin(playerInput, computerInput);
+		let message = "what?"
+		
+		switch (result) {
+			// win
+			case 1:
+			message = "win!";
+			wins += 1;
+			break;
+
+			// lose
+			case 2:
+			message = "lost!";
+			loses += 1;
+			break;
+
+			// tie
+			case 3:
+			message = "tied!";
+			ties += 1;
+			break;
+		}
+
+		console.log("You " + message + "\nYou: " + playerInput +
+			"\nComputer: " + computerInput);
+	}
+
+	if (wins > loses) {
+		console.log("You win! | " + wins + " to " + loses);
+	} else if (loses > wins) {
+		console.log("You lost the game :'( | " + wins + " to " + loses);
+	} else {
+		console.log("You somehow tied. Wow.");
+	}
+}
+
+playGame(5);
